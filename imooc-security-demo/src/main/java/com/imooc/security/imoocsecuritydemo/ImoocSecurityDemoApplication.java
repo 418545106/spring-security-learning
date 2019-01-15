02d45c32bb4fb27891189d74fad8644ca7148f4f
zhupandong <418545106@qq.com>
2019-01-15 13:59:20 +0800

    Initial commit

---
+++ imooc-security-demo/src/main/java/com/imooc/security/imoocsecuritydemo/ImoocSecurityDemoApplication.java
@@ -0,0 +1,14 @@
+package com.imooc.security.imoocsecuritydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImoocSecurityDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImoocSecurityDemoApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/com/imooc/security/imoocsecurity/ImoocSecurityApplication.java
@@ -0,0 +1,14 @@
+package com.imooc.security.imoocsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImoocSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImoocSecurityApplication.class, args);
+	}
+
+}
+
